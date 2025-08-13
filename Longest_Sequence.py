adb319b085b3f833bd4d3957b8d241edb85e08d2
Alexander Christensen <alexander@kmax.dk>
2025-08-13 10:55:23 +0200

    solved problem

--- Longest_Sequence.py
+++ Longest_Sequence.py
@@ -1,8 +1,20 @@
 from typing import List
 
+
 def longestConsecutive(nums: List[int]) -> int:
+    longest_sequence = 0
     myset = set(nums)
 
-    return myset
+    for num in myset:
+        if num - 1 not in myset:
+            start = num
+            streak = 1
+            while start + streak in myset:
+                streak += 1
+            longest_sequence = max(longest_sequence, streak)
 
-print (longestConsecutive([1,100,3,4]))
+    return longest_sequence
+
+print(longestConsecutive([100,4,200,1,3,2]))
+print(longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
+print(longestConsecutive([1,0,1,2]))
