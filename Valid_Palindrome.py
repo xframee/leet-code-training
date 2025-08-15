f73493c3a1ee579d621109b9ae75c7bf0f0346fa
Alexander Christensen <alexander@kmax.dk>
2025-08-15 09:15:20 +0200

    added alternative solution

--- Valid_Palindrome.py
+++ Valid_Palindrome.py
@@ -7,6 +7,18 @@ def isPalindrome(s: str) -> bool:
             return False
     return True
 
+def isPalindrome2(s: str) -> bool:
+    s = s.lower()
+    s = ''.join([char for char in s if char.isalnum()])
+
+    s2 = s[::-1]
+
+    return s == s2
+
 print (isPalindrome("A man, a plan, a canal: Panama"))
 print (isPalindrome("race a car"))
 print (isPalindrome(" "))
+
+print (isPalindrome2("A man, a plan, a canal: Panama"))
+print (isPalindrome2("race a car"))
+print (isPalindrome2(" "))
