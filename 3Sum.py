20d76ac7e5210d7b35801d10f7e4b5b695c13584
xframee <54414538+xframee@users.noreply.github.com>
2025-08-17 09:24:35 +0200

    completed question

--- 3Sum.py
+++ 3Sum.py
@@ -5,7 +5,8 @@ def threeSum(nums: List[int]) -> List[List[int]]:
     res = []
 
     for i in range(len(nums)):
-
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
         l, r = i + 1, len(nums) - 1
         while l < r:
             s = nums[i] + nums[l] + nums[r]
@@ -13,15 +14,17 @@ def threeSum(nums: List[int]) -> List[List[int]]:
                 l += 1
 
             elif s > 0:
-                r += 1
+                r -= 1
 
             else:
                 res.append([nums[i], nums[l], nums[r]])
-                # 2) advance l and r while skipping duplicates
-                #    e.g., while l<r and nums[l]==nums[l+1]: l += 1
-                #          while l<r and nums[r]==nums[r-1]: r -= 1
-                # then move both l += 1, r -= 1
-                ...
+                
+                while l < r and nums[l] == nums[l+1]:
+                    l += 1
+                while l < r and nums[r] == nums[r-1]:
+                    r -= 1
+                    
+                l += 1; r -= 1
     return res
 
 
