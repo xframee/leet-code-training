e4e670415a83364331199361843d0372b19da17e
Alexander Christensen <alexander@kmax.dk>
2025-08-18 10:54:20 +0200

    brute-force solution works but too slow

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -1,4 +1,14 @@
 from typing import List
 
 def maxArea(height: List[int]) -> int:
-    pass
+    max_area = 0
+
+    for i in range (len(height)):
+        for j in range (i+1, len(height)):
+            area = min(height[i], height[j]) * (j-i)
+            if area > max_area:
+                max_area = area
+
+    return max_area
+
+print (maxArea([1,8,6,2,5,4,8,3,7]))
