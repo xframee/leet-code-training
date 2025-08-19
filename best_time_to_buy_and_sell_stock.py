881bf36890df8835cd4a27f1d4fa3b46c8656bc6
xframee <54414538+xframee@users.noreply.github.com>
2025-08-19 13:55:31 +0200

    added sliding window approach

--- best_time_to_buy_and_sell_stock.py
+++ best_time_to_buy_and_sell_stock.py
@@ -10,5 +10,18 @@ def maxProfit(prices: List[int]) -> int:
             profit = price - min_price
     return profit
 
-print (maxProfit([7,1,5,3,6,4]))
-print (maxProfit([7,6,4,3,1]))
+def maxProfit2(prices: List[int]) -> int: #sliding window apprach
+    l, r = 0, 1
+    max_profit = 0
+
+    while r < len(prices):
+        if prices[l] < prices[r]:
+            profit = prices[r] - prices[l]
+            max_profit = max(max_profit, profit)
+        else:
+            l = r
+        r += 1
+    return max_profit
+
+print (maxProfit2([7,1,5,3,6,4]))
+print (maxProfit2([7,6,4,3,1]))
