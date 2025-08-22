02de52ed666b7b069b94d1ef3ff16403b4f8b8ab
Alexander Christensen <alexander@kmax.dk>
2025-08-22 12:06:03 +0200

    solved problem

--- longest_repeating_char_replace.py
+++ longest_repeating_char_replace.py
@@ -1,2 +1,18 @@
 def characterReplacement(s: str, k: int) -> int:
-    pass
+    count = {}
+    res = 0 
+    
+    l = 0
+    for r in range (len(s)):
+        count [s[r]] = 1 + count.get(s[r], 0)
+
+        while (r - l + 1 ) - max(count.values()) > k:
+            count[s[l]] -= 1
+            l += 1
+
+        res = max(res, r - l + 1)
+
+    return res
+
+print (characterReplacement("ABAB", 2))
+print (characterReplacement("AABABBA", 1))
