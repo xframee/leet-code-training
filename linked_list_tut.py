0acca000e455d8f453032e573e57252d0d28ee79
Alexander Christensen <alexander@kmax.dk>
2025-08-27 10:12:57 +0200

    added doubly linked

--- linked_list_tut.py
+++ linked_list_tut.py
@@ -57,3 +57,50 @@ def search(Head, val):
 print(search(Head, 2))
 print(search(Head, 7))
 print(search(Head, 1))
+
+#Doubly Linkedlists
+
+class DoublyNode:
+    def __init__(self, val, next=None, prev=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+
+    def __str__(self):
+        return str(self.val)
+    
+head = tail = DoublyNode(1)
+print(head)
+print(tail)
+
+#Display - O(n)
+def display(head):
+    curr = head
+    elements = []
+    while curr:
+        elements.append(str(curr.val))
+        curr = curr.next
+    print(" <-> ".join(elements))
+
+display(head)
+
+#Insert at the beginning O(1)
+
+def insert_at_beginning(head, tail, val):
+    new_node = DoublyNode(val, next=head)
+    head.prev = new_node
+    return new_node, tail
+
+head, tail = insert_at_beginning(head, tail, 3)
+display(head)
+
+#Insert at end O(1)
+
+def insert_at_end(head, tail, val):
+    new_node = DoublyNode(val, prev=tail)
+    tail.next = new_node
+    return head, new_node
+
+head, tail = insert_at_end(head, tail, 7)
+display(head)
+    
