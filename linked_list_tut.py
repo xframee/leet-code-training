09d0d6660e9d391cfafea098c46d55f23aeaa498
Alexander Christensen <alexander@kmax.dk>
2025-08-27 09:52:26 +0200

    update linkedlist features

---
+++ linked_list_tut.py
@@ -0,0 +1,30 @@
+# There are two types: Singly linked and doulby linked lists
+# Single ones only point forwards, double points both ways
+
+#Sinlgy Linked Lists:
+
+class SinglyNode:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        return str(self.val)
+    
+Head = SinglyNode(1)
+A = SinglyNode(3)
+B = SinglyNode(4)
+C = SinglyNode(7)
+
+Head.next = A
+A.next = B
+B.next = C
+
+print(Head)
+
+#Traverse the list - O(n)
+curr = Head
+
+while curr:
+    print (curr)
+    curr = curr.next
