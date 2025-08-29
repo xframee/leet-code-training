dde9e2b3d23079f2f9b4e3215aa8310a71e8c92d
Alexander Christensen <alexander@kmax.dk>
2025-08-29 11:45:42 +0200

    added problem

---
+++ reverse_linked_list.py
@@ -0,0 +1,7 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+    pass
